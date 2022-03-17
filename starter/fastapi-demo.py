82fc08c48f3f916b48c23d9be90d53314e3005c8
AmberJing88 <j289xu@uwaterloo.ca>
2022-03-16 22:14:26 -0300

    add two features

--- starter/fastapi-demo.py
+++ starter/fastapi-demo.py
@@ -1,1 +1,20 @@
 # the demo of the fastapi 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Value(BeseModel):
+    value: int
+        
+        
+@app.post("/{path}")
+async def exercise_function(path: int, query:int, body:Value):
+    return {"path": path, "query":query, "body": body}
+
+
+@app.get("/items/{item_id}")
+async def get_items(item_id: int, count: int = 1):
+    return {"fetch": f"Fetched {count} of {item_id}"}
+
